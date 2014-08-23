8aa3329f0b469f7ff2e370d16adabaa93b35f4f7
Eugene Marcotte <eugene@emarcotte.com>
2014-08-23 14:17:12 +0000

    Start

---
+++ src/GameResources.js
@@ -0,0 +1,29 @@
+function _Image( name, path ) {
+    return { name: name, type: "image", src: "data/" + name + ".png" };
+}
+
+function _Audio( name ) {
+    return { name: name, type: "audio", src: "data/" , channels: 2 };
+}
+
+function _Level( name ) {
+    return { name: name, type: "tmx", src: "data/" + name + ".tmx" };
+}
+
+var GameResources = [
+    /* Radmars Logo */
+    _Image( "intro_bg" ),
+    _Image( "intro_glasses1" ),
+    _Image( "intro_glasses2" ),
+    _Image( "intro_glasses3" ),
+    _Image( "intro_glasses4" ),
+    _Image( "intro_mars" ),
+    _Image( "intro_radmars1" ),
+    _Image( "intro_radmars2" ),
+    _Audio( "radmarslogo" ),
+
+    //_Audio( "beep", "data/" ),
+    _Image( "mapshit1"),
+    _Image( "collision_tiles" ),
+    _Level( "level1" )
+];
---
+++ src/LD30.js
@@ -0,0 +1,162 @@
+var screenHeight = 600;
+var screenWidth = 800;
+
+var LD30 = function() {
+
+    /**
+     * Start stuff when the page loads.
+     */
+    this.onload = function() {
+        if ( !me.video.init( 'canvas', screenWidth, screenHeight ) ) {
+            alert ("Yer browser be not workin");
+        }
+
+        me.audio.init ("m4a,ogg" );
+
+        // Sync up post loading stuff.
+        me.loader.onload = this.loaded.bind( this );
+
+        me.loader.preload( GameResources );
+
+        me.state.change( me.state.LOADING );
+        return;
+    };
+
+    /**
+     * Do stuff post-resource-load.
+     */
+    this.loaded = function() {
+
+        me.state.set( me.state.INTRO, new RadmarsScreen() );
+        me.state.set( me.state.PLAY, new PlayScreen() );
+
+      //  me.state.change( me.state.INTRO );
+        me.state.change( me.state.PLAY );
+        //me.debug.renderHitBox = false;
+
+        // me.pool.register( "player", Player );
+
+    };
+};
+
+/** The game play state... */
+var PlayScreen = me.ScreenObject.extend({
+    init: function() {
+        this.parent( true );
+    },
+
+    getLevel: function() {
+        return this.parseLevel( me.levelDirector.getCurrentLevelId() );
+    },
+
+    parseLevel: function( input ) {
+        var re = /level(\d+)/;
+        var results = re.exec( input );
+        return results[1];
+    },
+
+    /** Update the level display & music. Called on all level changes. */
+    changeLevel: function( level ) {
+        // this only gets called on start?
+        //me.audio.stopTrack();
+        me.game.world.sort();
+        me.game.viewport.fadeOut( '#000000', 1000, function() {
+         //   me.audio.playTrack( level );
+        });
+    },
+
+    // this will be called on state change -> this
+    onResetEvent: function() {
+        var level =  location.hash.substr(1) || "level1" ;
+        me.levelDirector.loadLevel( level );
+        this.changeLevel( level );
+    },
+
+    onDestroyEvent: function() {
+       // me.audio.stopTrack();
+    },
+
+    update: function() {
+        me.game.frameCounter++;
+    }
+});
+
+var RadmarsScreen = me.ScreenObject.extend({
+    onResetEvent: function() {
+        this.radmars = new RadmarsRenderable();
+        me.game.world.addChild( this.radmars );
+
+        this.subscription = me.event.subscribe( me.event.KEYDOWN, function (action, keyCode, edge) {
+            if( keyCode === me.input.KEY.ENTER ) {
+                me.state.change( me.state.MENU );
+            }
+        });
+
+        me.audio.playTrack( "radmarslogo" );
+    },
+
+    onDestroyEvent: function() {
+        me.input.unbindKey(me.input.KEY.ENTER);
+        me.audio.stopTrack();
+        me.game.world.removeChild( this.radmars );
+        me.event.unsubscribe( this.subscription );
+    }
+});
+
+var RadmarsRenderable = me.Renderable.extend({
+    init: function() {
+        this.parent( 0, screenHeight, screenWidth );
+        this.counter = 0;
+
+        this.floating = true;
+
+        if( !this.title ) {
+            this.bg= me.loader.getImage("intro_bg");
+            this.glasses1 = me.loader.getImage("intro_glasses1"); // 249 229
+            this.glasses2 = me.loader.getImage("intro_glasses2"); // 249 229
+            this.glasses3 = me.loader.getImage("intro_glasses3"); // 249 229
+            this.glasses4 = me.loader.getImage("intro_glasses4"); // 249 229
+            this.text_mars = me.loader.getImage("intro_mars"); // 266 317
+            this.text_radmars1 = me.loader.getImage("intro_radmars1"); // 224 317
+            this.text_radmars2 = me.loader.getImage("intro_radmars2");
+        }
+
+        me.input.bindKey( me.input.KEY.ENTER, "enter", true );
+    },
+
+    draw: function(context) {
+        context.drawImage( this.bg, 0, 0 );
+        if( this.counter < 130) context.drawImage( this.text_mars, 266+80, 317+60 );
+        else if( this.counter < 135) context.drawImage( this.text_radmars2, 224+80, 317+60 );
+        else if( this.counter < 140) context.drawImage( this.text_radmars1, 224+80, 317+60 );
+        else if( this.counter < 145) context.drawImage( this.text_radmars2, 224+80, 317+60 );
+        else if( this.counter < 150) context.drawImage( this.text_radmars1, 224+80, 317+60 );
+        else if( this.counter < 155) context.drawImage( this.text_radmars2, 224+80, 317+60 );
+        else if( this.counter < 160) context.drawImage( this.text_radmars1, 224+80, 317+60 );
+        else if( this.counter < 165) context.drawImage( this.text_radmars2, 224+80, 317+60 );
+        else context.drawImage( this.text_radmars1, 224+80, 317+60 );
+
+        if( this.counter < 100) context.drawImage( this.glasses1, 249+80, 229*(this.counter/100)+60 );
+        else if( this.counter < 105) context.drawImage( this.glasses2, 249+80, 229+60 );
+        else if( this.counter < 110) context.drawImage( this.glasses3, 249+80, 229+60 );
+        else if( this.counter < 115) context.drawImage( this.glasses4, 249+80, 229+60 );
+        else context.drawImage( this.glasses1, 249+80, 229+60 );
+    },
+
+    update: function( dt ) {
+        if ( this.counter < 350 ) {
+            this.counter++;
+        }
+        else{
+            me.state.change(me.state.MENU);
+        }
+        // have to force redraw :(
+        me.game.repaint();
+    }
+});
+
+
+window.onReady(function() {
+    window.app = new LD30();
+    window.app.onload();
+});
